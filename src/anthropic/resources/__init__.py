485a97ea7f1fef13feb8c6f12ee7fc0f1851118a
Wes Higbee <wes.mcclure@gmail.com>
2026-02-28 00:13:39 -0600

    move plumbing for lazy load

--- src/anthropic/resources/__init__.py
+++ src/anthropic/resources/__init__.py
@@ -1,4 +1,6 @@
 # File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+_lazy_from_imports: dict[str, str] = {}
+_lazy_module_imports: dict[str, str] = {}
 
 from .beta import (
     Beta,
@@ -59,3 +61,31 @@ __all__ = [
     "BetaWithStreamingResponse",
     "AsyncBetaWithStreamingResponse",
 ]
+
+
+def __dir__() -> list[str]:
+    return sorted(
+        set(globals().keys())
+            .union(_lazy_from_imports.keys())
+    )
+
+
+def __getattr__(name: str) -> object:
+    if name in _lazy_module_imports:
+        import importlib
+        module = importlib.import_module(_lazy_module_imports[name], __spec__.parent)
+        globals()[name] = module
+        return module
+
+    if name in _lazy_from_imports:
+        import importlib 
+        # lazy loaded attrs on a module:
+        module = importlib.import_module(_lazy_from_imports[name], __spec__.parent)
+        value = getattr(module, name)
+        try:
+            value.__module__ = "anthropic"
+        except (TypeError, AttributeError):
+            pass
+        globals()[name] = value
+        return value
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
